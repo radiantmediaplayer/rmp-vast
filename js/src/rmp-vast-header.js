662de49451176b7f4338b3e68a7f068ce32bdadb
Arnaud Leyder <arnaud@radiantmediaplayer.com>
2017-04-27 19:06:08 +0200

    License + demo link update

--- js/src/rmp-vast-header.js
+++ js/src/rmp-vast-header.js
@@ -2,5 +2,5 @@
  * @license Copyright (c) 2017 Radiant Media Player | https://www.radiantmediaplayer.com
  * rmp-vast 0.1.0
  * GitHub: https://github.com/radiantmediaplayer/rmp-vast
- * MIT License: https://www.radiantmedialyzer.net/rmp-vast/mit-license.html
+ * MIT License: https://github.com/radiantmediaplayer/rmp-vast/blob/master/LICENSE
  */
