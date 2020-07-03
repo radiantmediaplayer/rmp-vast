01d00d395909325d07c56e986cd0f2834ad9476c
radiantmediaplayer <arnaud@radiantmediaplayer.com>
2020-07-03 15:58:08 +0200

    2.4.15
    - updates dependencies
    - removes --source-map from builds

--- js/dist/rmp-vast.js
+++ js/dist/rmp-vast.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2017-2020 Radiant Media Player | https://www.radiantmediaplayer.com
- * rmp-vast 2.4.14
+ * rmp-vast 2.4.15
  * GitHub: https://github.com/radiantmediaplayer/rmp-vast
  * MIT License: https://github.com/radiantmediaplayer/rmp-vast/blob/master/LICENSE
  */
