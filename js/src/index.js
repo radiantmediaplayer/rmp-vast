6181808aa6140c163875a3821ee12dd7b081fc38
radiantmediaplayer <arnaud@radiantmediaplayer.com>
2020-11-20 11:11:50 +0100

    tick version to 3.0.2

--- js/src/index.js
+++ js/src/index.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2017-2020 Radiant Media Player | https://www.radiantmediaplayer.com
- * rmp-vast 3.0.1
+ * rmp-vast 3.0.2
  * GitHub: https://github.com/radiantmediaplayer/rmp-vast
  * MIT License: https://github.com/radiantmediaplayer/rmp-vast/blob/master/LICENSE
  */
